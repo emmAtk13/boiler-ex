9f596ac31ea5d0024afa74ac38a2670461c89b26
Emma <35468679+emmAtk13@users.noreply.github.com>
2019-03-04 14:52:16 -0500

    works more on the nav menu
    implements the first java script code for the collapsable menu, modifies the grid

--- js/page.js
+++ js/page.js
@@ -8,3 +8,12 @@
 //function changeColor(){
 //  document.getElementById('color').classList.add('red');
 //}
+
+function navToggle() { // creates a new function named navToggle
+  var x = document.getElementById("main-nav"); // creates a variable set to #main-nav
+    if (x.className === "topnav") { // if #main-nav has a class of .topnav,
+    x.className += " responsive";   // then add a new class
+  } else {                          // if it doesn't,
+    x.className = "topnav";         // set class to .topnav
+  } //closes if-else
+} //
